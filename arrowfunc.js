e83d97ea536ddd21e7ae09f8b9d45e8ebe2e0d1b
Mrinal <mail.mrinal10@gmail.com>
2022-01-28 14:25:10 +0530

    first commit

---
+++ arrowfunc.js
@@ -0,0 +1,11 @@
+const quName ='mrinal'
+let myAge = 25
+
+
+const summarizeUsers = (name, uAge) => {
+    return (
+        uAge+" : "+name
+    );
+};
+
+console.log(summarizeUsers(quName, myAge));
